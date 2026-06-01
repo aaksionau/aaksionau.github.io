71a54f1c66424cd653d0878d0ff7fed4ba563c47
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2026-06-01 18:13:23 -0500

    Remove links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -112,14 +112,6 @@ const config: Config = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
               label: 'X',
               href: 'https://x.com/docusaurus',
             },
@@ -134,12 +126,12 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/aaksionau',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Aliaksei Aksionau. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
